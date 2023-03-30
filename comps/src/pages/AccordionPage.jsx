1b272054f971cd0ae2c6934cd7b26fb091e5ade6
Douglas Silva <douglas.souza@venturus.org.br>
2023-03-30 10:30:51 -0300

    [Section 12] Project organization

---
+++ comps/src/pages/AccordionPage.jsx
@@ -0,0 +1,28 @@
+import Accordion from '../components/Accordion';
+
+function AccordionPage() {
+  const items = [
+    {
+      id: 's126s12',
+      label: 'label 1',
+      content:
+        'content 1 content 1 content 1 content 1 content 1 content 1 content 1 content 1',
+    },
+    {
+      id: '2s2123',
+      label: 'label 2',
+      content:
+        'content 2 content 2 content 2 content 2 content 2 content 2 content 2 content 2',
+    },
+    {
+      id: '12s2134',
+      label: 'label 3',
+      content:
+        'content 3 content 3content 3content 3content 3content 3content 3content 3content 3',
+    },
+  ];
+
+  return <Accordion items={items} />;
+}
+
+export default AccordionPage;
