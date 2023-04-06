b4ee7885e05fe64b3070d193aebd3c8908dad7d2
Douglas Silva <douglas.souza@venturus.org.br>
2023-04-06 14:54:14 -0300

    [Section 18] Reducers

--- comps/src/pages/CounterPage.jsx
+++ comps/src/pages/CounterPage.jsx
@@ -1,12 +1,119 @@
+import produce from 'immer';
+import { useReducer } from 'react';
 import Button from '../components/Button';
-import useCounter from '../hooks/use-counter';
+import Panel from '../components/Panel';
+
+const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'decrement';
+const SET_VALUE_TO_ADD = 'set-value-to-add';
+const ADD_VALUE_TO_COUNT = 'add-value-to-count';
+//
+// WITHOUT IMMER WE SHOULD USE IT!!!!!!
+//
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case INCREMENT_COUNT:
+//       return {
+//         ...state,
+//         count: state.count + 1,
+//       };
+//     case DECREMENT_COUNT:
+//       return {
+//         ...state,
+//         count: state.count - 1,
+//       };
+//     case SET_VALUE_TO_ADD:
+//       return {
+//         ...state,
+//         valueToAdd: action.payload,
+//       };
+//     case ADD_VALUE_TO_COUNT:
+//       return {
+//         ...state,
+//         count: state.count + state.valueToAdd,
+//         valueToAdd: 0,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+//
+// ONLY WHEN USING IMMER!!!!!!
+//
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count++;
+      return;
+    case DECREMENT_COUNT:
+      state.count--;
+      return;
+    case SET_VALUE_TO_ADD:
+      state.valueToAdd = action.payload;
+      return;
+    case ADD_VALUE_TO_COUNT:
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
+    default:
+      return;
+  }
+};
 
 export default function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter(initialCount);
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+
+  const [state, dispatch] = useReducer(produce(reducer), {
+    count: initialCount,
+    valueToAdd: 0,
+  });
+
+  const increment = () => {
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
+  };
+  const decrement = () => {
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
+  };
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
+  };
+
   return (
-    <div>
-      <h1>Count is {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg">Count is {state.count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          onChange={handleChange}
+          value={state.valueToAdd || ''}
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add it!</Button>
+      </form>
+    </Panel>
   );
 }
