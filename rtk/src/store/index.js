9e11f0e7a157727190804c30bad938730e6d16f9
Douglas Silva <douglas.souza@venturus.org.br>
2023-04-10 10:23:22 -0300

    [Section 19] Redux initial code

--- rtk/src/store/index.js
+++ rtk/src/store/index.js
@@ -19,4 +19,17 @@ const store = configureStore({
   },
 });
 
-console.log(store);
+const startingState = store.getState();
+console.log(JSON.stringify(startingState));
+
+// Without Action creators
+// store.dispatch({
+//   type: 'song/addSong',
+//   payload: 'New Song!!!',
+// });
+
+//With action creators, just a shortcut for the same thing
+store.dispatch(songsSlice.actions.addSong('Some song!'));
+
+const finalState = store.getState();
+console.log(JSON.stringify(finalState));
